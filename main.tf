3cc48e869e9b65c92dd76cbf2c8649e8094257dd
Ken Lee <ken.lee@nokia.com>
2020-06-05 00:21:17 +0800

    Provisioned simple instance
    serving hello world on port 8080.

--- main.tf
+++ main.tf
@@ -1,12 +1,29 @@
 provider "aws" {
-	region = "ap-northeast-1"
+  region = "ap-northeast-1"
 }
 
-resource "aws_instance" "my_instance" {
-	ami = "ami-0278fe6949f6b1a06"
-	instance_type = "t2.micro"
-	
-	tags = {
-		"Name" = "terraform-example"
-	}
+resource "aws_instance" "server_instance" {
+  ami                    = "ami-0278fe6949f6b1a06"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.server_secgroup.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "hello world!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    "Name" = "server-instance"
+  }
+}
+
+resource "aws_security_group" "server_secgroup" {
+  name = "server-secgroup"
+  ingress {
+    from_port   = 8080
+	to_port     = 8080
+	protocol    = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+  }
 }
