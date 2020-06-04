fba065955915995cc9f02b3d0d9599cea41d9437
Ken Lee <ken.lee@nokia.com>
2020-06-05 01:04:29 +0800

    Added server_port variable

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
+variable "server_port" {
+  description = "Server port for http request"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_instance" "server_instance" {
   ami                    = "ami-0278fe6949f6b1a06"
   instance_type          = "t2.micro"
@@ -10,7 +16,7 @@ resource "aws_instance" "server_instance" {
   user_data = <<-EOF
               #!/bin/bash
               echo "hello world!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   tags = {
@@ -21,9 +27,14 @@ resource "aws_instance" "server_instance" {
 resource "aws_security_group" "server_secgroup" {
   name = "server-secgroup"
   ingress {
-    from_port   = 8080
-	to_port     = 8080
+    from_port   = var.server_port
+	to_port     = var.server_port
 	protocol    = "tcp"
 	cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+output "public_ip" {
+  description = "IP address of web server"
+  value = aws_instance.server_instance.public_ip
 }
