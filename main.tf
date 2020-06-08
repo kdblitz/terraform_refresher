a6f781fc3242e096bce4f02e6ac7ef8e0ef29954
Ken Lee <ken.lee@nokia.com>
2020-06-08 13:04:58 +0800

    Added load balancer
    Resource linked to auto scaling group.

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_launch_configuration" "server_config" {
   security_groups = [aws_security_group.server_secgroup.id]
 
   user_data = <<-EOF
-              #!/bin/bash  
+              #!/bin/bash
               echo "Hello world!" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
@@ -46,6 +46,9 @@ resource "aws_autoscaling_group" "server_asg" {
   launch_configuration = aws_launch_configuration.server_config.name
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
 
+  target_group_arns = [aws_lb_target_group.server_asg.arn]
+  health_check_type = "ELB"
+
   min_size = 2
   max_size = 10
 
@@ -56,10 +59,80 @@ resource "aws_autoscaling_group" "server_asg" {
   }
 }
 
-/**
-output "public_ip" {
-  description = "IP address of web server"
-  value = aws_instance.server_instance.public_ip
+resource "aws_lb" "server_lb" {
+  name               = "simple-server-alb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb_secgroup.id]
 }
 
-*/
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.server_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb_secgroup" {
+  name = "simple-server-alb-secgroup"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "server_asg" {
+  name = "simple-server-asg"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "server_lb" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type  = "forward"
+    target_group_arn = aws_lb_target_group.server_asg.arn
+  }
+}
+
+output "alb_dns" {
+  description = "DNS of load balancer for web server"
+  value = aws_lb.server_lb.dns_name
+}
